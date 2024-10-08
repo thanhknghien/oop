645bc19a09912fb2870f7f5451f81480020dba71
thanhknghien <144438575+thanhknghien@users.noreply.github.com>
2024-10-08 22:12:37 +0700

    Delete src/main/java directory

--- src/main/java/Modify.java
+++
@@ -1,8 +0,0 @@
-public interface Modify<T> {
-    void add(T item);
-    void edit(T item);
-    void delete(int id);
-    void importData(String filePath);
-    void exportData(String filePath);
-    void view();
-}
